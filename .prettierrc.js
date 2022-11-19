80f700262871d532a74639e82e62c0d3ba90470d
rnfltpgus <jpp901220@gmail.com>
2022-11-19 21:32:23 +0900

    ⚙️ Feat: CRA 후 초기 환경 셋팅(ESLint, Prettier 설정, tyconfig.json 설정추가)

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  parser: 'typescript',
+  semi: true,
+  useTabs: false,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+};
